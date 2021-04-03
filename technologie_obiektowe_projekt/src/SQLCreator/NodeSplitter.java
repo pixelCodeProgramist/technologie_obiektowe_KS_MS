6711ba8f75a120c902872d345244a11e91ca59f0
karol.skorek <karol.skorek>
2021-04-04 01:42:47 +0200

    Generowanie kodu sql na postawie diagramu bez powiazan

---
+++ technologie_obiektowe_projekt/src/SQLCreator/NodeSplitter.java
@@ -0,0 +1,44 @@
+package SQLCreator;
+
+import ERDCreator.resources.XTableView;
+import javafx.scene.control.Label;
+import models.MoveableNodeModel;
+import models.TableModel;
+
+import java.util.List;
+
+public class NodeSplitter {
+    private MoveableNodeModel moveableNodeModel;
+    private Label label;
+    private XTableView xTableView;
+    private List<TableModel> rowsOfTable;
+
+    public NodeSplitter(MoveableNodeModel moveableNodeModel) {
+        this.moveableNodeModel = moveableNodeModel;
+        this.label = this.moveableNodeModel.getLabel();
+        this.xTableView = this.moveableNodeModel.getxTableView();
+        this.rowsOfTable = (List<TableModel>) this.xTableView.getItems();
+    }
+
+    private String getTableName() {
+        return label.getText();
+    }
+
+    public String buildQuery(){
+        StringBuilder query = new StringBuilder();
+        query.append("CREATE TABLE "+getTableName()+" (\n");
+        this.rowsOfTable.forEach(row->{
+            query.append(row.getId()+" "+row.getType());
+            if(row.isPrimaryKey()) query.append(" NOT NULL PRIMARY KEY");
+            if(!row.isPrimaryKey()){
+                if(row.isUnique()) query.append(" UNIQUE");
+                if(row.isNotNull()) query.append(" NOT NULL");
+            }
+            if(!row.equals(rowsOfTable.get(rowsOfTable.size()-1))) query.append(",\n");
+        });
+        query.append(");");
+        return query.toString();
+    }
+
+
+}
--- technologie_obiektowe_projekt/src/SQLCreator/SQLCreatorController.java
+++ technologie_obiektowe_projekt/src/SQLCreator/SQLCreatorController.java
@@ -3,17 +3,31 @@ package SQLCreator;
 import Config.Configuration;
 import ERDCreator.ERDCreatorController;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import models.MoveableNodeModel;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import java.util.Set;
 
-public class SQLCreatorController{
+public class SQLCreatorController implements Initializable {
 
+    @FXML
+    private TextArea textAreaID;
+    private ArrayList<NodeSplitter> nodeSplitters;
     private Set<MoveableNodeModel> nodes;
     public SQLCreatorController(Set<MoveableNodeModel> nodes) {
         this.nodes = nodes;
+        this.nodeSplitters = new ArrayList<>();
     }
 
     @FXML
@@ -21,4 +35,20 @@ public class SQLCreatorController{
         Configuration configuration = new Configuration();
         configuration.changeScene("../ERDCreator/ERDCreator.fxml", mouseEvent, new ERDCreatorController(nodes));
     }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.textAreaID.setEditable(false);
+        buildListNodeSpitters();
+    }
+
+    private void buildListNodeSpitters(){
+        this.nodes.forEach(node ->{
+            NodeSplitter nodeSplitter = new NodeSplitter(node);
+            this.textAreaID.setText(this.textAreaID.getText()+nodeSplitter.buildQuery());
+        });
+    }
+
+
 }
